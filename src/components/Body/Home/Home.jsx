d143ab4b5e2982a4d12debeb1adcb905ed794133
lilpinc <133043171+lilpinc@users.noreply.github.com>
2024-05-07 21:28:33 -0400

    moved around the text-align to make it more clean

--- src/components/Body/Home/Home.jsx
+++ src/components/Body/Home/Home.jsx
@@ -11,15 +11,15 @@ export default function Home() {
             entries.forEach((entry) => {
                 if (entry.isIntersecting) {
                     entry.target.classList.add('show');
+                    observer.unobserve(entry.target)
                 } else {
                     entry.target.classList.remove('show');
                 }
             })
         });
 
-     hiddenElementsRef.current.forEach((el) => observer.observe(el));
+        hiddenElementsRef.current.forEach((el) => observer.observe(el));
     }, [hiddenElementsRef.current]);
-            
 
 
 
@@ -27,65 +27,78 @@ export default function Home() {
         <>
             <main>
                 <header className="home" id="home">
-                    <h1 className="summary">Hi, I'm Anna Britta!</h1>
+                    <h1 id="itsame" className="summary">Hi, I'm Anna Britta!</h1>
                     <p className="size summary">I am a full stack web developer who enjoys creating aesthetic designs with efficient and easy to read code.</p>
                 </header>
                 <section id="about" ref={(el) => hiddenElementsRef.current.push(el)} className="hidden">
                     <h1><b>About</b></h1>
-                    <p className="school-info paragraphs">I want to talk all about me!</p>
+                    <div className='summary summary2'>
+                        <p className="school-info paragraphs">I want to talk all about me!</p>
+                    </div>
                 </section>
                 <section id="skills" ref={(el) => hiddenElementsRef.current.push(el)} className="hidden">
                     <h1><b>Skills</b></h1>
-                    <ul className="school-info paragraphs"><b>Languages</b>
-                        <div className="languages">
-                            <li className="skills">SQL</li>
-                            <li className="skills">mySQL</li>
-                            <li className="skills">GraphQL</li>
-                            <li className="skills">MongoDB</li>
-                            <li className="skills">Jaascript (ES6)</li>
-                            <li className="skills">HTML</li>
-                            <li className="skills">CSS</li>
+                    <div className='stack summary summary2'>
+                        <div className="school-info paragraphs"><b>Languages</b>
+                            <div className="languages">
+                                <li className="skills">SQL</li>
+                                <li className="skills">mySQL</li>
+                                <li className="skills">GraphQL</li>
+                                <li className="skills">MongoDB</li>
+                                <li className="skills">Jaascript (ES6)</li>
+                                <li className="skills">HTML</li>
+                                <li className="skills">CSS</li>
+                            </div>
                         </div>
-                    </ul>
-                    <ul className="school-info paragraphs"><b>Libraries & Frameworks</b>
-                        <div className="languages">
-                            <li className="skills">Node.js</li>
-                            <li className="skills">Express.js</li>
-                            <li className="skills">React.js</li>
-                            <li className="skills">Bootstrap</li>
+                        <div className="school-info paragraphs"><b>Libraries & Frameworks</b>
+                            <div className="languages">
+                                <li className="skills">Node.js</li>
+                                <li className="skills">Express.js</li>
+                                <li className="skills">React.js</li>
+                                <li className="skills">Bootstrap</li>
+                            </div>
                         </div>
-                    </ul>
-                    <ul className="school-info paragraphs"><b>Tools</b>
-                        <div className="languages">
-                            <li className="skills">Git</li>
-                            <li className="skills">Netlify</li>
-                            <li className="skills">Heroku</li>
-                            <li className="skills">Insomnia & Postman</li>
+                        <div className="school-info paragraphs"><b>Tools</b>
+                            <div className="languages">
+                                <li className="skills">Git</li>
+                                <li className="skills">Netlify</li>
+                                <li className="skills">Heroku</li>
+                                <li className="skills">Insomnia & Postman</li>
+                            </div>
                         </div>
-                    </ul>
+                    </div>
                 </section>
                 <section id="experience" ref={(el) => hiddenElementsRef.current.push(el)} className="hidden">
                     <h1><b>Experience</b></h1>
-                    <p className="school-info paragraphs"><b>Student</b></p>
-                    <p className="school-info paragraphs"><b>Kickoff</b></p>
-                    <p className="school-info paragraphs"><b>Exercise Therapist & Personal Trainer</b></p>
+                    <div className='summary summary2'>
+                        <p className="school-info paragraphs"><b>Full Stack Web Developer</b></p>
+                        <p className="school-info">University of Denver (2023)</p>
+                        <p className="school-info paragraphs"><b>Online Personal Trainer</b></p>
+                        <p className="school-info">Kickoff (2023-present)</p>
+                        <p className="school-info paragraphs"><b>In Person Personal Trainer</b></p>
+                        <p className="school-info">Anytime Fitness (2023-2024)</p>
+                        <p className="school-info paragraphs"><b>Exercise Therapist & Personal Trainer</b></p>
+                        <p className="school-info">Advanced Wellness Centre (2020-2023)</p>
+                    </div>
                 </section>
-                <section id="projects" className="hidden">
+                <section id="projects" ref={(el) => hiddenElementsRef.current.push(el)} className="hidden">
                     <h1><b>Projects</b></h1>
-                    <p className="school-info paragraphs projects"><b>OmniHealth Records</b></p>
-                    <p className="school-info paragraphs projects"><b>Project Hyperdrive</b></p>
-                    <p className="school-info paragraphs projects"><b>ABP Fitness</b></p>
+                    <div className='summary summary2'>
+                        <p className="school-info paragraphs projects"><b>OmniHealth Records</b></p>
+                        <p className="school-info paragraphs projects"><b>Project Hyperdrive</b></p>
+                        <p className="school-info paragraphs projects"><b>ABP Fitness</b></p>
+                    </div>
                 </section>
                 <section id="education" ref={(el) => hiddenElementsRef.current.push(el)} className="hidden hidden3">
                     <h1><b>Education</b></h1>
-                    <ul className='summary'>
+                    <div className='summary summary2'>
                         <p className="school-name ">University of Denver (2023) </p>
                         <p className="school-info">Full Stack Coding Boot Camp</p>
                         <p className="school-name">Old Dominion University (2018-2019)</p>
                         <p className="school-info">M.S. Exercise Science</p>
                         <p className="school-name">Virginia Polytechnic Institute and State University (2013-2017)</p>
                         <p className="school-info">B.S. Biological Sciences</p>
-                    </ul>
+                    </div>
                 </section>
             </main>
         </>
